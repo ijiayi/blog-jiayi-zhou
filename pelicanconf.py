6153da05aaba92844b7d61317440481d5cf8434d
Jiayi Zhou <mail4jiayi@gmail.com>
2014-03-27 20:32:31 +0800

    Update config

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = u'Jiayi Zhou'
 SITENAME = u'Jiayi Zhou'
-SITEURL = 'http://blog.ideaera.com'
+#SITEURL = 'http://jiayi.ideaera.com'
 
 TIMEZONE = 'Europe/Paris'
 
@@ -31,7 +31,11 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-# ==================================================
+# ==========================================================
+AUTHOR = u'Jiayi Zhou'
+SITENAME = u'Jiayi Zhou'
+SITEURL = 'http://jiayi.ideaera.com'
+
 TIMEZONE = 'Asia/Taipei'
 
 DEFAULT_DATE_FORMAT = "%Y-%m-%d"
@@ -43,7 +47,23 @@ PYGMENTS_RST_OPTIONS = {'linenos': 'none'}  # 'linenos': 'table'
 
 FILENAME_METADATA = r'(?P<date>\d{4}-\d{2}-\d{2})-(?P<slug>.*)'
 
+STATIC_PATHS = ['images', 'extra/hello.txt']
+EXTRA_PATH_METADATA = {
+    'extra/hello.txt': {'path': 'hello.txt'},
+}
+
+FEED_ATOM = 'feeds/atom.xml'
+#FEED_ALL_ATOM = 'feeds/all.atom.xml'
+
+
+# ==========================================================
+# Theme
+
+SITESUBTITLE = 'elegance maker'
+
 LINKS = ()
 SOCIAL = ()
 
-
+THEME = 'pelican-bootstrap3'
+THEME = 'pelican-simplegrey'
+THEME = 'plumage'
